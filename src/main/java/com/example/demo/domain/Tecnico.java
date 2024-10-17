08cabcb8109ffc93e62527f92a5b88124ba33686
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-10-17 13:45:40 -0300

    Classes de dominio iniciadas

---
+++ src/main/java/com/example/demo/domain/Tecnico.java
@@ -0,0 +1,25 @@
+package com.example.demo.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tecnico extends Pessoa{
+
+    private List<Chamado> chamados = new ArrayList<>();
+
+    public Tecnico() {
+       super();
+    }
+
+    public Tecnico(String nome, String cpf, String email, String senha, List<Chamado> chamados) {
+        super(nome, cpf, email, senha);
+    }
+
+    public List<Chamado> getChamados() {
+        return chamados;
+    }
+
+    public void setChamados(List<Chamado> chamados) {
+        this.chamados = chamados;
+    }
+}
