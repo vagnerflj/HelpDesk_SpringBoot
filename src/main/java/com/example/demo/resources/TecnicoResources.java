e286fb9cf853d2dd09963ddc4e758f0660c998d0
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-10-18 14:19:18 -0300

    Endpoint findAll para tecnico

--- src/main/java/com/example/demo/resources/TecnicoResources.java
+++ src/main/java/com/example/demo/resources/TecnicoResources.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value= "/tecnicos")
 public class TecnicoResources {
@@ -22,6 +25,12 @@ public class TecnicoResources {
     public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = service.findById(id);
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
+    }
+    @GetMapping
+    public ResponseEntity<List<TecnicoDTO>> findAll() {
+        List<Tecnico> list = service.findAll();
+        List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
