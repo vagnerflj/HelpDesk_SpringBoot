af7889c3ae6377e6339af51ae822f82b559f5531
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-10-18 11:40:01 -0300

    Excecao para objeto nao encontrado

---
+++ src/main/java/com/example/demo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.demo.resources.exceptions;
+
+import com.example.demo.services.exceptions.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex, HttpServletRequest request) {
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+                "Object Not Found", ex.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+}
--- src/main/java/com/example/demo/services/TecnicoService.java
+++ src/main/java/com/example/demo/services/TecnicoService.java
@@ -2,6 +2,7 @@ package com.example.demo.services;
 
 import com.example.demo.domain.Tecnico;
 import com.example.demo.repositories.TecnicoRepository;
+import com.example.demo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
     }
 }
